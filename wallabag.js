fc17aab9d081299fecf9541664f1d5e21ce7d169
Heiko Adams <heiko.adams@gmail.com>
2016-02-16 12:34:20 +0100

    Bugfix

--- wallabag.js
+++ wallabag.js
@@ -5,7 +5,7 @@
 		console.log(query);
 
 		var d = new Date();
-      var ts = d.getTime();
+		var ts = d.getTime();
 
 		var w = window.open('backend.php?op=backend&method=loading', 'ttrss_wallabag',
 			"status=0,toolbar=0,location=0,width=600,height=500,scrollbars=1,menubar=0");
@@ -14,8 +14,7 @@
 			parameters: query,
 			onComplete: function(transport) {
 				var ti = JSON.parse(transport.responseText);
-
-				var share_url = ti.wallabag_url+"/index.php?action=add&plainurl=" + param_escape(ti.link);
+				var share_url = ti.wallabag_url+"/index.php?action=add&autoclose=true&plainurl=" + param_escape(ti.link);
 
 				w.location.href = share_url;
 
